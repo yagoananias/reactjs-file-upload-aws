337714037b788c9ddd12e0132df0ce2878b28b01
Yago Ananias <yago_souza92@yahoo.com.br>
2021-09-27 20:30:15 -0300

    Front end added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component, component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    selectFile: null,
+    fileUploadedSuccessfully: false
+  }
+
+  onFileChange = event => {
+    this.setState({selectedFile: event.target.files[0]});
+  }
+
+  onFileUpload = () => {
+    const formData = new FormData();
+    formData.append(
+      "demo file",
+      this.state.selectFile,
+      this.state.selectFile.name
+    )
+    //call api
+    console.log(formData);
+    this.setState({selectedFile: null});
+    this.setState({fileUploadedSuccessfully: true});
+  }
+
+  fileData = () => {
+    if (this.state.selectedFile) {
+      return (
+      <div>
+        <h2>File Details:</h2>
+        <p>File name:{this.state.selectedFile.name}</p>
+        <p>File type:{this.state.selectedFile.type}</p>
+        <p>Last modified:{" "}{this.state.selectedFile.lastModifiedDate.toDateString()}</p>
+      </div>
+      );
+    } else if (this.state.fileUploadedSuccessfully) {
+      return(
+        <div>
+          <br />
+          <h4>Your file has been succefully uploaded!</h4>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose a file and then press the Upload button</h4>
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h2>File Upload System</h2>
+        <h3>Upload with React.js and a Serverless API!</h3>
+        <div>
+          <input type="file" onChange={this.onFileChange} />
+          <button onClick={this.onFileUpload}>
+            Upload
+          </button>
+        </div>
+        {this.fileData()}
+      </div>
+    )
+  }
 }
 
 export default App;
